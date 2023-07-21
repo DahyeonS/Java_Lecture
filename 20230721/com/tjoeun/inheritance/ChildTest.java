d2ef6d3c87319ed4b931f87d598b7c7c42507ad3
Dahyeon Seo <88021405+DahyeonS@users.noreply.github.com>
2023-07-21 16:52:04 +0900

    Add files via upload

--- 20230721/com/tjoeun/inheritance/ChildTest.java
+++ 20230721/com/tjoeun/inheritance/ChildTest.java
@@ -22,5 +22,60 @@ public class ChildTest extends ParentTest {
 //		super();
 		System.out.println("자식 클래스의 생성자 실행");
 	}
+
+//	이클립스가 지원하는 생성자 자동 완성 기능을 사용할 때 현재 클래스에 물리적으로 정의하지 않은
+//	필드(상속받은 필드)는 생성자를 만드는 대화상자에 표시되지 않는다.
+	
+//	부모 클래스에서 상속받은 private 권한의 필드를 초기화하는 방법은 부모 클래스에서 상속받은
+//	필드의 접근 권한이 private일 경우 자식 클래스에서 접근할 수 없으므로 부모 클래스의 생성자를
+//	호출해서 초기화시킨다.
+//	public ChildTest(String name, boolean gender, int age, String nickName) {
+//		super(name, gender); // ParentTest(String name, boolean gender)가 실행
+//		this.name = name; // 에러, 부모클래스에서 private로 설정된 필드는 접근할 수 없다.
+//		this.age = age;
+//		this.nickName = nickName;
+//	}
+	
+//	부모 클래스의 private 권한으로 설정된 모든 필드의 setter가 정의되어 있다면 setter의
+//	접근 권한은 메소드이므로 public을 사용하고 public 권한은 자식 클래스에서 실행할 수 있으므로
+//	부모 클래스에서 상속받는 private 권한의 필드를 setter를 이용해서 초기화시킬 수 있
+//	public ChildTest(String name, boolean gender, int age, String nickName) {
+//		super();
+//		setName(name);
+//		setGender(gender);
+//		this.age = age;
+//		this.nickName = nickName;
+//	}
+	
+//	부모 클래스에서 상속받은 필드의 접근 권한이 protected이면 자식 클래스에서 접근할 수
+//	있으므로 this를 사용하여 초기화가 가능하다.
+	public ChildTest(String name, boolean gender, int age, String nickName) {
+		super();
+		this.name = name;
+		this.gender = gender;
+		this.age = age;
+		this.nickName = nickName;
+	}
+
+//	Override(재정의): 부모 클래스로부터 상속받은 자식 클래스
+	@Override
+	public String toString() {
+//		부모 클래스에서 상속받은 private 권한을 가지는 필드는 자식에서 접근할 수 없으므로
+//		부모 클래스에서 상속받은 private 권한을 가지는 필드에 저장된 데이터를 getter 메소드를
+//		사용해서 접근해야 한다.
+//		리턴 타입이 boolean인 메소드는 "is"로 메소드 이름을 시작하는 게 관행이다.
+//		return getName() + "(" + (isGender() ? "남" : "여") + ") - " + age + ", " + nickName;
+		
+//		자식 클래스에서 부모 클래스에서 상속받은 메소드의 앞, 뒤에 기능을 추가할 수 있다.
+//		부모 클래스에서 상속받은 메소드를 실행하려면 부모 클래스를 의미하는 super 뒤에 "."을 찍고
+//		실행할 부모 클래스의 메소드 이름을 쓰면 된다.
+//		super.toString():  부모 클래스의 toString() 메소드를 실행한다.
+//		return super.toString();
+		
+//		부모 클래스에서 상속받은 필드의 접근 권한이 protected일 경우 자식 클래스에서 접근할 수
+//		있으므로 getter를 사용하지 않아도 필드의 내용을 얻어올 수 있다.
+		return name + "(" + (gender ? "남" : "여") + ") - " + age + ", " + nickName;
+	}
+
 	
 }
--- 20230721/com/tjoeun/inheritance/ParentTest.java
+++ 20230721/com/tjoeun/inheritance/ParentTest.java
@@ -3,8 +3,11 @@ package com.tjoeun.inheritance;
 // 부모(상위, 슈퍼, 기반) 클래스
 public class ParentTest {
 	
-	private String name;
-	private boolean gender;
+//	private String name;
+//	private boolean gender;
+	
+	protected String name;
+	protected boolean gender;	
 	
 //	기본생성자가 실행되면 name은 "기타", gender는 false로 초기화한다.
 	public ParentTest() {
@@ -40,6 +43,7 @@ public class ParentTest {
 	}
 
 	@Override
+//	홍길동(남)
 	public String toString() {
 		return "ParentTest [name=" + name + ", gender=" + gender + "]";
 	}
