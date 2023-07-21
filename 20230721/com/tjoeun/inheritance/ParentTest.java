cafff548096dd44f35d0fcf45abf857b96521dac
Dahyeon Seo <88021405+DahyeonS@users.noreply.github.com>
2023-07-21 16:10:10 +0900

    Add files via upload

---
+++ 20230721/com/tjoeun/inheritance/ParentTest.java
@@ -0,0 +1,15 @@
+package com.tjoeun.inheritance;
+
+// 부모(상위, 슈퍼, 기반) 클래스
+public class ParentTest {
+	
+	private String name;
+	private boolean gender;
+	
+//	기본생성자가 실행되면 name은 "기타", gender는 false로 초기화한다.
+	public ParentTest() {
+		name = "기타";
+		gender = false;
+	}
+
+}
